58804432f525252b5632ff6bedf20d30ad1823bb
Server <minecraft-srv@v22018025835161770.hotsrv.de>
2018-03-09 21:16:54 +0100

    styles;scripts;pages;fonts;everything

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ function setCookie(cookieName, siteLang) {
     var expire = new Date();
     expire.setTime(today.getTime() + 3600000*24*7);
     document.cookie = cookieName + "="+ escape(siteLang) + ";expires=" + expire.toGMTString();
-	update(document);
+  update(document);
 }
 
 function getCookie(cookieName) {
@@ -48,9 +48,19 @@ function applyPage(doc) {
     console.log("Page applied: " + used);
 }
 
+function checkPage(element) {
+    var attrib = element.getAttribute("page");
+    var used = getCookie("page");
+    if (!used) {
+        used = "main"
+    }
+    return attrib == null || attrib == used || attrib == "any";
+}
+
 function update(doc) {
     applyLanguage(doc);
-	applyPage(doc);
+    applyPage(doc);
+    HideScrollBar("container2");
 }
 
 
@@ -73,12 +83,13 @@ function loadScript(url, callback) {
 function includeHTML() {
   var z, i, elmnt, file, xhttp;
   /*loop through a collection of all HTML elements:*/
-  z = document.getElementsByTagName("*");
+  z = document.querySelectorAll('[include-html]');
   for (i = 0; i < z.length; i++) {
     elmnt = z[i];
     /*search for elements with a certain atrribute:*/
     file = elmnt.getAttribute("include-html");
-    if (file) {
+    if (file && checkPage(elmnt)) {
+      console.log("LOAD: " + file);
       /*make an HTTP request using the attribute value as the file name:*/
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
@@ -88,7 +99,7 @@ function includeHTML() {
           /*remove the attribute, and call this function once more:*/
           elmnt.removeAttribute("include-html");
           includeHTML();
-		  update(elmnt);
+      update(elmnt);
         }
       } 
       xhttp.open("GET", file, true);
@@ -98,9 +109,17 @@ function includeHTML() {
     }
   }
 }
-
 includeHTML();
 
+function HideScrollBar(containerName) {
+    var containers = document.getElementsByClassName(containerName);
+
+    for (i in containers) {
+        if (containers[i].style) {
+            containers[i].style.paddingRight = containers[i].offsetWidth - containers[i].clientWidth + "px";
+        }
+    }
+}
 
 function payment() {
     var sheet = window.document.styleSheets[0];
@@ -120,6 +139,5 @@ function description(item) {
     }
     update(document);
 }
-
 
 $(document).ready(update(document));
